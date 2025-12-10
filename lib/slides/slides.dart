34f9503e866ce18b88bc959846b62dde50f14aca
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-12-10 16:47:48 +0900

    🚧 タイムライン入りスライドを作成途中

--- lib/slides/slides.dart
+++ lib/slides/slides.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_deck/flutter_deck.dart';
+import 'package:gbdaitokai_2025/slides/src/opening/flutter_timeline_slide.dart';
 import 'package:gbdaitokai_2025/slides/src/opening/icebreak_slide.dart';
 import 'package:gbdaitokai_2025/slides/src/opening/title_slide.dart';
 
 List<FlutterDeckSlideWidget> get slides => [
   const IcebreakSlide(),
   const TitleSlide(),
+  const FlutterTimelineSlide(),
 ];
