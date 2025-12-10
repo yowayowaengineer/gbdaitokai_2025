ce18f58a7f0895ffef09efcf9878244bcb9b12f7
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2025-12-10 17:28:00 +0900

    ✨ Flutterの成長スライドを作成

--- lib/slides/slides.dart
+++ lib/slides/slides.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_deck/flutter_deck.dart';
+import 'package:gbdaitokai_2025/slides/src/opening/flutter_growth_slide.dart';
 import 'package:gbdaitokai_2025/slides/src/opening/flutter_timeline_slide.dart';
 import 'package:gbdaitokai_2025/slides/src/opening/icebreak_slide.dart';
 import 'package:gbdaitokai_2025/slides/src/opening/title_slide.dart';
@@ -7,4 +8,5 @@ List<FlutterDeckSlideWidget> get slides => [
   const IcebreakSlide(),
   const TitleSlide(),
   const FlutterTimelineSlide(),
+  const FlutterGrowthSlide(),
 ];
